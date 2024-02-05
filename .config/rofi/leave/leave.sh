65caad865e923f7b73e64d5dbde45b381719ccd7
HackTheOxidation <tomas.hagenau@protonmail.ch>
2024-02-05 11:06:58 +0100

    Update rofi logout script

--- .config/rofi/leave/leave.sh
+++ .config/rofi/leave/leave.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+wallpaper=/usr/share/backgrounds/personal/stairs.jpg
+
 choice=$(printf "Lock\nLogout\nSuspend\nReboot\nShutdown" | rofi -dmenu)
 if [[ $choice == "Lock" ]];then
-    bash ~/.config/system_scripts/wayland_session_lock
+    swaylock --image $wallpaper
 elif [[ $choice == "Logout" ]];then
     pkill -KILL -u "$USER"
 elif [[ $choice == "Suspend" ]];then
